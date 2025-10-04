d107436e647edf27c78b07d32db021276eb2e30b
MuhammadYusuf <110709884+muhammadyusufxon404@users.noreply.github.com>
2025-10-04 13:39:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def index():
     <html lang="uz">
     <head>
         <meta charset="utf-8">
-        <title>Maktab CRM</title>
+        <title>MAGISTR BAZA</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <style> body { background: #f0f2f5; } </style>
